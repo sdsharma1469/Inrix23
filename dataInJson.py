55a362083e8bc787fbbeeb1a488c8e7c7e428b60
sdsharma1469 <sdsharma@scu.edu>
2023-11-12 09:06:28 -0800

    final

--- dataInJson.py
+++ dataInJson.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, make_response
 import requests
 import lat_long
 import json
@@ -7,33 +7,29 @@ import pandas as pd
 import time
 from flask_cors import CORS, cross_origin
 app = Flask(__name__)
-CORS(app)
+app.app_context().push()
 
+CORS(app)
 CORS(app, resources={r"/getOffStreetParking": {"origins": "http://localhost:8000"}})
 app.config['CORS_HEADERS'] = 'Content-Type'
 app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
 
 @app.route('/gettoken')
-@cross_origin()
 def get_token():
     app_id = "5ncryh0565"
     hash_token = "NW5jcnloMDU2NXxFUG10d1NEVGJwNHhCSzJWQjdqMlk1NGhoRFhjYjRaUDVwTkcxUXJY"
     url = f"https://api.iq.inrix.com/auth/v1/appToken?appId={app_id}&hashToken={hash_token}"
-    try:
-        response = requests.get(url)
-        response.raise_for_status()
-        json_data = response.json()
-        token = json_data['result']['token']
-        print("token", token)
-        return token
+    response = requests.get(url)
+    response.raise_for_status()
+    json_data = response.json()
+    token = json_data['result']['token']
+    print("MYYYY TOKENNN" + token)
+    return token
 
-    except requests.exceptions.RequestException as e:
-        return jsonify(error=str(e)), 500
 
 # Open the JSON file
 
 import http.client
-from datetime import datetime, timedelta
 
 
 def TradeAreaTrips(address):
@@ -44,7 +40,7 @@ def TradeAreaTrips(address):
     headers = {
             'accept': 'application/json',
             'Authorization': "Bearer "+str(get_token()),
-            'Access-Control-Allow-Origin': 'http://127.0.0.1:9000',
+            'Access-Control-Allow-Origin': 'http://127.0.0.1:8000',
             'Access-Control-Allow-Credentials': 'true'
         }
     input_string = location
@@ -67,7 +63,7 @@ def getOffStreetParking(address):
         headers = {
             'accept': 'application/json',
             'Authorization': "Bearer "+token_x,
-            'Access-Control-Allow-Origin': 'http://127.0.0.1:9000',
+            'Access-Control-Allow-Origin': 'http://127.0.0.1:8000',
             'Access-Control-Allow-Credentials': 'true'
         }
 
@@ -85,6 +81,8 @@ def getOffStreetParking(address):
         return str(count)
     except Exception as e:
         return jsonify(error=str(e)), 500
+    
+
     
 @app.route('/GroupByZip', methods=['GET'])
 @cross_origin()
